39d30dd623d4f40e5e8a6f5ef7c7ee40d5bdd1d6
habib <ha9.0bib90@gmail.com>
2022-11-15 02:27:07 +0100

    add cponfig300

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/ihebfront2'));
+app.use(express.static(__dirname + '/dist/IhebFront2'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/ihebfront2/index.html'));});
+'/dist/IhebFront2/index.html'));});
 app.listen(process.env.PORT || 3000);
